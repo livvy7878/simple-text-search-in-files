486095439f2fc9b00a6b5075201acc16ad146ea1
livvy7878 <live7878787@gmail.com>
2020-12-13 22:56:42 +0200

    Main view model added

---
+++ SimpleSearchInTextFiles/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace SimpleSearchInTextFiles
+{
+	internal class MainViewModel : DependencyObject
+	{
+		public static readonly DependencyProperty CurrentPathToSearchProperty = DependencyProperty.Register(
+			"CurrentPathToSearch", typeof(string), typeof(MainViewModel), new PropertyMetadata(default(string)));
+
+		public static readonly DependencyProperty CurrentTextToSearchProperty = DependencyProperty.Register(
+			"CurrentTextToSearch", typeof(string), typeof(MainViewModel), new PropertyMetadata(default(string)));
+
+		public string CurrentPathToSearch
+		{
+			get => (string) GetValue(CurrentPathToSearchProperty);
+			set => SetValue(CurrentPathToSearchProperty, value);
+		}
+
+		public string CurrentTextToSearch
+		{
+			get => (string) GetValue(CurrentTextToSearchProperty);
+			set => SetValue(CurrentTextToSearchProperty, value);
+		}
+	}
+}
